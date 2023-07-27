59873667decf81d575586bde895ff026d7950584
rahsin01 <140702610+rahsin01@users.noreply.github.com>
2023-07-27 15:12:39 +0530

    Add files via upload

---
+++ Worker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityToSQL
+{
+    class Worker: BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Caller caller = new Caller();
+            caller.Call();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
+}
